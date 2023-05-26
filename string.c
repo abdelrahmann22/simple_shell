d81373a47759f163fc2d487b707e93fe0ce651d5
abdelrahmann22 <boudyali22@gmail.com>
2023-05-26 14:32:05 +0300

    Check

--- string.c
+++ string.c
@@ -104,3 +104,35 @@ char *_strdup(char *str)
 	result[i] = '\0';
 	return (result);
 }
+
+/**
+ * _strcpy - copies a string
+ * @dest: the destination string
+ * @src: the source string
+ * Return: (Success) a pointer to the destination string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: the character to be checked
+ * Return: (Success) 1 if c is a letter, 0 otherwise
+*/
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
